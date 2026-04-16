4ff4903f7e4f4aa7fe9e84e851033d687deaa51a
lee <zif000901@gmail.com>
2026-04-16 12:32:53 +0900

    inti: 백엔드 초기 설정

---
+++ backend/src/main/java/com/sprint/daonil/DaonilApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.daonil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaonilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaonilApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sprint/daonil/DaonilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.daonil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaonilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
