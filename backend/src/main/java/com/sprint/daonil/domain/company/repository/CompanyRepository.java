167d5fc93814f62fc0eb2f36ffa02b71b8490a77
lee <zif000901@gmail.com>
2026-04-21 22:34:25 +0900

    #17 perf: 공고 조회 관련 n+1 -> @EntityGraph 적용

--- backend/src/main/java/com/sprint/daonil/domain/company/repository/CompanyRepository.java
+++ backend/src/main/java/com/sprint/daonil/domain/company/repository/CompanyRepository.java
@@ -1,6 +1,7 @@
 package com.sprint.daonil.domain.company.repository;
 
 import com.sprint.daonil.domain.company.entity.Company;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,7 @@ import java.util.Optional;
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     boolean existsByBusinessNumber(String businessNumber);
+
+    @EntityGraph(attributePaths = {"member"})
     Optional<Company> findByMember_LoginId(String loginId);
 }
