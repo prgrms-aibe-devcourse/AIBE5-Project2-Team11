17e1f37ff8d99ed5fdaadf4a612372e8a61cae29
Hwangbohye <hwangbohye98@gmail.com>
2026-04-26 16:14:50 +0900

    게시글&댓글 신고 api

---
+++ backend/src/main/java/com/sprint/daonil/domain/report/repository/ReportRepository.java
@@ -0,0 +1,7 @@
+package com.sprint.daonil.domain.report.repository;
+
+import com.sprint.daonil.domain.report.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
