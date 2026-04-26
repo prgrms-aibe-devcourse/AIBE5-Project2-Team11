f6928c81dcdc17a47b6e7e34753837d0d75b14ea
송지훈 <songjihunny@gmail.com>
2026-04-26 15:25:06 +0900

    #30 feat/birth_date
    2026/04/26-feat/birth_date commit

--- backend/src/main/java/com/sprint/daonil/domain/member/dto/ProfileRequestDto.java
+++ backend/src/main/java/com/sprint/daonil/domain/member/dto/ProfileRequestDto.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Builder
 public class ProfileRequestDto {
 
+    private String birthDate;
     private String career;
     private String preferredJob;
     private String preferredRegion;
